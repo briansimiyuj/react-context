ee854524ac2e34b8e5cb38f0883470d1dde6f4b7
Brian Simiyu <briansimiyuj@gmail.com>
2024-07-02 10:00:54 +0300

    Creating multiple context

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,6 +1,7 @@
 import BookList from "./components/BookList"
 import Navbar from "./components/Navbar"
 import ThemeToggle from "./components/ThemeToggle"
+import AuthContextProvider from "./context/AuthContext"
 import ThemeContextProvider from "./context/ThemeContext"
 
 function Script(){
@@ -11,11 +12,15 @@ function Script(){
 
       <ThemeContextProvider>
 
-        <Navbar/>
+        <AuthContextProvider>
 
-        <BookList/>
+          <Navbar/>
 
-        <ThemeToggle/>
+          <BookList/>
+
+          <ThemeToggle/>
+
+        </AuthContextProvider>
 
       </ThemeContextProvider>
 
